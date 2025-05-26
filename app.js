e8cd836a7e650f212b2571807824c05551a8dcdb
chicoticc <leoarnd04@gmail.com>
2025-05-26 11:05:45 -0400

    Se crea la primera ruta GET/info que devuelve mensaje de bienvenida

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 
 app.listen(4000, console.log("Servidor activo"));
+
+app.get("/info", (req, res) => {
+    res.send("Bienvenido al servidor de evaluación");
+})

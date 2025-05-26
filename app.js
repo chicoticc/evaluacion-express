e94bd7bc15cf22988862642e525799b14df29443
chicoticc <leoarnd04@gmail.com>
2025-05-26 11:30:38 -0400

    Se agrega la ruta POST/productos y middleware

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const app = express();
 
 app.listen(4000, console.log("Servidor activo"));
 
+app.use(express.json());
+
 app.get("/info", (req, res) => {
     res.send("Bienvenido al servidor de evaluación");
 });
 
 app.get("/productos", (req, res) => {
-    const productos = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    const productos = JSON.parse(fs.readFileSync("productos.json"));
     res.send(productos);
+});
+
+app.post("/productos", (req, res) => {
+    const productoIngresado = req.body;
+    const productos = JSON.parse(fs.readFileSync("productos.json"));
+    productos.push(productoIngresado);
+    fs.writeFileSync("productos.json", JSON.stringify(productos));
+    res.send("Producto agregado exitosamente");
 });

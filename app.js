87e1235f0011b0a5bca513e9c495fc4c241d7a68
chicoticc <leoarnd04@gmail.com>
2025-05-26 11:13:49 -0400

    Se agrega ruta GET/productos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("node:fs");
 
 const app = express();
 
@@ -6,4 +7,9 @@ app.listen(4000, console.log("Servidor activo"));
 
 app.get("/info", (req, res) => {
     res.send("Bienvenido al servidor de evaluación");
-})
+});
+
+app.get("/productos", (req, res) => {
+    const productos = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
+    res.send(productos);
+});
